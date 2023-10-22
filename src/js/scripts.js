a5abb0f6b46f8825cf85467372f5b50e5d8511d3
bdinsite <bdinsight.oy@gmail.com>
2023-10-22 15:43:09 +0300

    Add functionality to the Add Project -button

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,8 @@
 const input = document.querySelector('input');
+
+const button = document.querySelector('button');
 const cardSearch = document.querySelectorAll('.project');
-input.addEventListener('change', wordSearch);
+button.addEventListener('click', wordSearch);
 
 function wordSearch() {
     const word = input.value.toLowerCase();
@@ -20,4 +22,45 @@ function wordSearch() {
     
     console.log(word);
     
+}
+
+const projectCards = document.querySelector('.projectCards');
+
+const addItem = document.querySelector('.addItem');
+
+addItem.addEventListener('click', addNewProject);
+
+function addNewProject() {
+    const newProject = document.createElement('div');
+    newProject.classList = 'project';
+    const h4 = document.createElement('h4');
+    const p = document.createElement('p');
+
+    const newCardLink = document.createElement('div');
+    newCardLink.classList = 'cardLinks';
+    const favouriteStar = document.createElement('img');
+    favouriteStar.src = './src/img/star-plus-outline.svg';
+    favouriteStar.classList = 'miniIcon favorite';
+    favouriteStar.alt = 'Favorite icon';
+
+    const siteLink = document.createElement('a');
+    siteLink.src = './src/img/link-variant.svg';
+    siteLink.class = 'miniIcon';
+
+    const codeLink = document.createElement('a');
+    codeLink.src = './src/img/eye-arrow-right-outline.svg';
+    codeLink.classList = 'miniIcon';
+
+    h4.textContent = 'New Project 1';
+    p.textContent = 'A whole lot of lorem ipsum and other cool stuff.'
+
+    newCardLink.appendChild(favouriteStar);
+    newCardLink.appendChild(codeLink);
+    newCardLink.appendChild(siteLink);
+
+    newProject.appendChild(h4);
+    newProject.appendChild(p);
+    newProject.appendChild(newCardLink);
+
+    projectCards.appendChild(newProject);
 }
